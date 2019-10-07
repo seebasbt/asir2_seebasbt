dda1ebc3784745cafb41ad15acdf0071a3a6af9c
seebasbt <seebasbt.sebas_bt00@hotmail.com>
2019-10-07 16:58:21 +0200

    2a.php

---
+++ 2a.php
@@ -0,0 +1,45 @@
+<?php
+
+$a=22;
+
+$b=14;
+
+echo '<div>';
+
+echo 'Suma de '.$a.' + '.$b.' = ';
+
+echo $a+$b;
+
+echo '</div>';
+
+
+
+echo '<div>';
+
+echo 'Resta de '.$a.' - '.$b.' = ';
+
+echo $a-$b;
+
+echo '</div>';
+
+
+
+echo '<div>';
+
+echo 'Multiplicación de '.$a.' x '.$b.' = ';
+
+echo $a*$b;
+
+echo '</div>';
+
+
+
+echo '<div>';
+
+echo 'División de '.$a.' / '.$b.' = ';
+
+echo $a/$b;
+
+echo '</div>';
+
+?>
--- catalogo.php
+++ catalogo.php
@@ -1,27 +1,27 @@
 <html>
 
-<h1>Enlaces de LInkedin de los Compañeros</h1>
+<h1>Enlaces de Linkedin</h1>
 
-<a href="https://github.com/Junnyper">-Kimberly-</a></br>
+<a href="https://github.com/Junnyper">Kimberly</a></br>
 
 <a href="https://www.linkedin.com/in/pablo-argumosa-ruiz/">Pablo</a></br>
 
 <a href="https://www.linkedin.com/in/davidjpblanco/">David</a></br>
 
 <a href="https://www.linkedin.com/in/jaimegarciacifuentes/">Jaime</a></br>
-<a href="https://github.com/serran23/asir2_alvaro">-Alvaro-</a></br>
+<a href="https://github.com/serran23/asir2_alvaro">Alvaro</a></br>
 
-<a href="https://github.com/jooseluis7">-Jose-</a></br>
+<a href="https://github.com/jooseluis7">Jose</a></br>
 
-<a href="https://github.com/delasen99">-Alejandro-</a></br>
+<a href="https://github.com/delasen99">Alejandro</a></br>
 
 <a href="https://www.linkedin.com/in/luis-camus-obregon">Luis</a></br>
 
-<a href="https://github.com/DanielHernandezCantera">-DanielH-</a></br>
+<a href="https://github.com/DanielHernandezCantera">DanielH</a></br>
 
 <a href="https://www.linkedin.com/in/kevin-diaz-tomero/">Kevin</a></br>
 
 
-<p>Una de las maneras de agilizar el proceso de recoleccion de links seria con la creacion de un formulario</p>
+<p>Decroly</p>
 
 </html>
